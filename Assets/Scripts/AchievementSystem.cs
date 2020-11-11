bcc3fe4eb7033910c76bef2e527a0aa5a98f1709
ragozh <ragozh@gmail.com>
2020-11-11 14:32:32 +0700

    Completed

---
+++ Assets/Scripts/AchievementSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AchievementSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        ObjectCollide.OnSubjectEntered += ObjectCollide_OnSubjectEntered;
+
+        // Bonus
+        string CountKey = "Number of collided";
+        string CountKeyFlag = CountKey + "flag";
+        PlayerPrefs.SetInt(CountKeyFlag, 0);
+    }
+
+    private void ObjectCollide_OnSubjectEntered(ObjectCollide objectCollide)
+    {
+        string individualKey = "Individual Achievement " + objectCollide.ObjName;
+        if (PlayerPrefs.GetInt(individualKey) == 1)
+            return;
+        PlayerPrefs.SetInt(individualKey, 1);
+        Debug.Log("Achivement " + objectCollide.ObjName);
+
+        // Bonus
+        string CountKey = "Number of collided";
+        string CountKeyFlag = CountKey + "flag";
+        int count = PlayerPrefs.GetInt(CountKey);
+        count++;
+        PlayerPrefs.SetInt(CountKey, count);
+        if (count > 5 && PlayerPrefs.GetInt(CountKeyFlag) == 0)
+        {
+            Debug.Log("Collide more than 5 achieved");
+            PlayerPrefs.SetInt(CountKeyFlag, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectCollide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectCollide : MonoBehaviour
+{
+    public static event Action<ObjectCollide> OnSubjectEntered;
+    [SerializeField]
+    public string ObjName { get { return gameObject.name; } }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (OnSubjectEntered != null)
+            OnSubjectEntered(this);
+        
+        DestroyObject();
+    }
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
